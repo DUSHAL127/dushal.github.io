3a92a047802ae953ff37a34f2cde6549ea3c152e
DUSHAL127 <dushal1272001@gmail.com>
2025-03-24 17:14:08 +0530

    Updated portfolio with new changes

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -15,12 +15,6 @@ export class NavbarComponent implements OnInit {
   onWindowScroll() {
     this.isScrolled = window.scrollY > 50;
   }
-/*************  ✨ Codeium Command ⭐  *************/
-  /**
-   * Set isScrolled to true if window is scrolled more than 50px from top, false otherwise.
-   * This is used to conditionally add the "scrolled" class to the navbar, which changes its appearance.
-   */
-/******  00de378e-aa26-4264-b032-a3ea5899cf51  *******/
   toggleMenu() {
     this.isMenuActive = !this.isMenuActive;
   }
